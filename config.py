d57571de60aac2448d514aa63b482ecd480582fd
foggyheaddd <kate.kabalevskaya@email.com>
2025-11-07 05:05:20 +0300

    vgv

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8495367324:AAHNP5u3wrRRCxUBOa6VAi3xNZi_ctKaO0U")
+GOOGLE_SHEET_KEY = os.getenv("GOOGLE_SHEET_KEY", "1rW615gmemSRK-vaQx5C6sGbv1feLH8V_LaLYAd_7rP0")
+
+# === Видео (ВСТАВЬ СВОЙ file_id!) ===
+VIDEO_POSITIVE = "BAACAgIAAxkBAAICN2kL1bz7VXNK91QuiB02wKYnBUxxAAKxiQACHTxgSPMGU-HfQZlKNgQ"  # ← от @RawDataBot, блок "video"
+VIDEO_NEGATIVE = "BAACAgIAAxkBAAICOWkL1esI1rvKs6R0ESGJkd3UBA9lAAKyiQACHTxgSEsnAoj7IYE9NgQ"  # ← от @RawDataBot, блок "video"
