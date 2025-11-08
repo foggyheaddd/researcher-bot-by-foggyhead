f9123fb22328dfa07929a3a59925db1c04b8c49c
foggyheaddd <kate.kabalevskaya@email.com>
2025-11-08 03:05:25 +0300

    Обновил requirements.txt

--- config.py
+++ config.py
@@ -6,3 +6,19 @@ GOOGLE_SHEET_KEY = os.getenv("GOOGLE_SHEET_KEY", "1rW615gmemSRK-vaQx5C6sGbv1feLH
 VIDEO_POSITIVE = "BAACAgIAAxkBAAICN2kL1bz7VXNK91QuiB02wKYnBUxxAAKxiQACHTxgSPMGU-HfQZlKNgQ"  # ← от @RawDataBot, блок "video"
 VIDEO_NEGATIVE = "BAACAgIAAxkBAAICOWkL1esI1rvKs6R0ESGJkd3UBA9lAAKyiQACHTxgSEsnAoj7IYE9NgQ"  # ← от @RawDataBot, блок "video"
 
+#import json
+
+
+#def get_google_credentials_json_string(file_path="credentials.json"):
+
+    #with open(file_path, "r", encoding="utf-8") as f:
+     #   data = json.load(f)
+    # json.dumps автоматически удаляет переносы и форматирует как одну строку
+    #json_string = json.dumps(data, separators=(',', ':'))
+   # return json_string
+
+
+# Пример использования:
+#if __name__ == "__main__":
+ #   result = get_google_credentials_json_string("credentials.json")
+  #  print(result)
